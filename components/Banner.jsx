581992ab4c846b89a4f3d59132250c81dab884a6
salvinramesh <salvinramesh@gmail.com>
2025-10-20 21:49:43 +0530

    coupon fix

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -9,13 +9,13 @@ export default function Banner() {
     const handleClaim = () => {
         setIsOpen(false);
         toast.success('Coupon copied to clipboard!');
-        navigator.clipboard.writeText('NEW20');
+        navigator.clipboard.writeText('NEW10');
     };
 
     return isOpen && (
         <div className="w-full px-6 py-1 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A]">
             <div className='flex items-center justify-between max-w-7xl  mx-auto'>
-                <p>Get 20% OFF on Your First Order!</p>
+                <p>Get 10% OFF on Your First Order!</p>
                 <div className="flex items-center space-x-6">
                     <button onClick={handleClaim} type="button" className="font-normal text-gray-800 bg-white px-7 py-2 rounded-full max-sm:hidden">Claim Offer</button>
                     <button onClick={() => setIsOpen(false)} type="button" className="font-normal text-gray-800 py-2 rounded-full">
