5c20e7f111a69cafa9720d421d429cf3ef0961af
salvinramesh <salvinramesh@gmail.com>
2025-10-20 11:01:41 +0530

    frontend fix

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
 
                         <div className='text-slate-800 text-sm font-medium mt-4 sm:mt-8'>
                             <p>Starts from</p>
-                            <p className='text-3xl'>{currency}99₹</p>
+                            <p className='text-3xl'>{currency}99</p>
                         </div>
 
                         {/* SHOP NOW button */}
